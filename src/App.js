a7f9b935989398a2444743ef9c20ce000c698758
carolainseija <carolainsilva1@gmail.com>
2022-04-13 10:56:02 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-//clase 177
-import React from 'react';
+import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  // const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
-    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films')
     const data = await response.json();
     const transformedMovies = data.results.map(res => {
@@ -21,9 +17,6 @@ function App() {
       }
     });
     setMovies(transformedMovies)
-    setIsLoading(false)
-    console.log(movies.id)
-    .catch() //
   }
 
   return (
@@ -32,8 +25,8 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        {/* {isLoading && <MoviesList movies={movies} />} */}
         <MoviesList movies={movies} />
+        {movies}
       </section>
     </React.Fragment>
   );
